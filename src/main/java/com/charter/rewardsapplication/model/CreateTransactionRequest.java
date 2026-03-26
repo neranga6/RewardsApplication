9e4602654ee8420de0454e3cfc66346d157c153b
neranga <nurapola@contractor.bbw.com>
2026-03-26 16:50:10 -0500

    Junit Issue fix

--- src/main/java/com/charter/rewardsapplication/model/CreateTransactionRequest.java
+++ src/main/java/com/charter/rewardsapplication/model/CreateTransactionRequest.java
@@ -8,7 +8,7 @@ public record CreateTransactionRequest(
         @JsonProperty("customer_id")
         Long customerId,
 
-        @JsonProperty("customer_name")
+        long l, @JsonProperty("customer_name")
         String customerName,
 
         @JsonProperty("amount")
