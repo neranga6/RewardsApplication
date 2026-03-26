827dbcce148cd08528b015c124dce578a190bbc0
neranga <nurapola@contractor.bbw.com>
2026-03-26 17:21:18 -0500

    Issue fix

--- src/main/java/com/charter/rewardsapplication/model/CreateTransactionRequest.java
+++ src/main/java/com/charter/rewardsapplication/model/CreateTransactionRequest.java
@@ -1,19 +1,10 @@
 package com.charter.rewardsapplication.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDate;
 
 public record CreateTransactionRequest(
-
-        @JsonProperty("customer_id")
         Long customerId,
-
-        @JsonProperty("customer_name")
         String customerName,
-
-        @JsonProperty("amount")
         Double amount,
-
-        @JsonProperty("transaction_date")
         LocalDate transactionDate
 ) {}
