7a60c6dcb32ab4007dc57d2d92ad18f8b83d93f1
neranga <nurapola@contractor.bbw.com>
2026-03-26 17:19:12 -0500

    Issue fix

--- src/main/java/com/charter/rewardsapplication/model/CustomerRewardsSummary.java
+++ src/main/java/com/charter/rewardsapplication/model/CustomerRewardsSummary.java
@@ -1,19 +1,10 @@
 package com.charter.rewardsapplication.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public record CustomerRewardsSummary(
-
-        @JsonProperty("customer_id")
         Long customerId,
-
-        @JsonProperty("customer_name")
         String customerName,
-
-        @JsonProperty("monthly_points")
         List<MonthlyPoints> monthlyPoints,
-
-        @JsonProperty("total_points")
         int totalPoints
 ) {}
